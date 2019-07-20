ed6b5fa8966cac9a3161779f525ac0f4ba3f9164
JC Smiley <jcsmileyjr@gmail.com>
2019-07-20 08:47:09 -0500

    Added Incident Types drop-down box

--- src/screen/Incident/Incident.js
+++ src/screen/Incident/Incident.js
@@ -8,11 +8,13 @@ import SubmitButton from '../../components/SubmitButton/SubmitButton';
 
 //names of the current casinos in Tunica
 const casinoNames = ["GoldStrike", "Horseshoe", "1st Jackpot", "Sam's Town", "Hollywood", "Fitz", "Isle of Capri"];
+const incidentTypes = ["Disputes", "Complaint", "Jackpot", "Crimminal", "Minor Gaming"];
 
 //Screen that allows the user to create a initial incident report
 export default function Incident(props) {
-    const [casino, setCasino] = useState("Casino Name");//react hook to update dropdown button title
-    
+    const [casino, setCasino] = useState("Casino Name");//react hook to update dropdown select casino button title
+    const [type, setType] = useState("Incident Type");//react hook to update dropdown select incident type button title
+
     return(
         <Container className="center">
             {/*Nav Bar */}
@@ -39,10 +41,15 @@ export default function Incident(props) {
             </section>
 
             {/*User will choose a incident type from a dropdown box */}
-            <section>
-                <Row><Col>Incident Type</Col></Row>            
-                <Row><Col></Col></Row>
-            </section> 
+            <section>           
+                <Row>
+                    <Col>
+                        <DropdownButton size="lg" id="showIncidentTypes" title={type} variant="success">
+                            {incidentTypes.map((type, index) =>(<Dropdown.Item key={index} onClick={()=>setType(type)}>{type}</Dropdown.Item>))}
+                        </DropdownButton>
+                    </Col>
+                </Row>
+            </section>
 
             {/*Submit Button to create incident and transfer user to active cases page */}
             <Row><Col><SubmitButton /></Col></Row>                                   
