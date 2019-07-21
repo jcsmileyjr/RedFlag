f60d365e7b3f4209d595e39e42ef8f1e1e5687e4
JC Smiley <jcsmileyjr@gmail.com>
2019-07-21 09:56:47 -0500

    Move data and logic concerning logging to a data file

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import {Container, Row} from 'react-bootstrap';
 
 import Login from "./screen/Login/login.js"; //Login page for all users
 import Incident from "./screen/Incident/Incident";//Incident report form to create a incident.
+import {CheckLogIn} from "./screen/Login/LoginData";
 
-const loginData = [{"username":"test", "pwd":"test", "auth":"agent"}, {"username":"jHenry123", "pwd":"hammer", "auth":"supervisor"}, {"username":"jAdam456", "pwd":"oldman", "auth":"agent"}];
+
 
 //notes from https://hackernoon.com/how-do-i-use-react-context-3eeb879169a2 on how to use React's Context
 const UserLogIn = React.createContext({});//Context for Login page and elements
@@ -24,20 +25,10 @@ class App extends Component {
     };
   }
 
-  //Method to check if the user enter username and pwd match a record in the database. If so, go to next screen based on authoration.
-  checkLogIn = () =>{
-    let passFail = false;//If true, then username/pwd is correct. initiate to false, 
-    const name = this.state.userName;//get the current username entered
-    const password = this.state.pwd;//get the current password entered
-    
-    //Loop through database of login credentials to check if entered username and pwd is legit. 
-    loginData.forEach(function(account){
-        if(account.username===name && account.pwd === password){
-          passFail = true;
-        }
-    });
-    
-    if(passFail === true){
+  //Method to confirm if the user enter username and pwd match a record in the database. If so, go to next screen based on authoration.
+  confirmLogIn = () =>{    
+    //calls a method from LoginData.js
+    if(CheckLogIn(this.state.userName, this.state.pwd)){
       this.setState({currentView: "incident"});
     }
   }
@@ -50,7 +41,7 @@ class App extends Component {
               <UserLogInProvider value={{
                 getUserName: (value)=> this.setState({userName:value}),
                 getPwd: (value) => this.setState({pwd:value}),
-                logIn: this.checkLogIn,
+                logIn: () => this.confirmLogIn(),
                 }}>
                 <Login />
               </UserLogInProvider> 
