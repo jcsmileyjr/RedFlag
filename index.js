d0a9f8a9863f96049a0ea4eb00009dd7e18fee04
JC Smiley <jcsmileyjr@gmail.com>
2019-07-30 19:46:21 -0500

    Can send login info to server, then data  back to client.

--- index.js
+++ index.js
@@ -11,10 +11,31 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 var incidents = require('./src/incidents.json');
+var creds = require('./src/credentials.json');
 
 app.get("/incidents", function(req, res){
 
-  res.json(incidents);
+  res.json(creds);
+  res.end();
+});
+
+app.post("/cred", function(req, res){
+  var userName = req.body.name;
+  var pwd = req.body.password;
+  var info = {};
+  
+  info.passFail = false;
+
+  console.log(userName + " : " + pwd);
+  creds.forEach( account => {
+    if(account.username === userName && account.pwd === pwd){
+      info.username = account.username;
+      info.auth = account.auth;
+      info.passFail = true;
+    }
+  });
+
+  res.json(info);
   res.end();
 });
 
