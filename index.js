632658e87c8b26a0f82f1f1850080393909ca544
JC Smiley <jcsmileyjr@gmail.com>
2019-08-01 06:59:26 -0500

    At log-in, list of reports is retrieved based on authorization level

--- index.js
+++ index.js
@@ -6,36 +6,56 @@ const app = express();
 app.use(express.static(path.join(__dirname, "/build")));
 
 //configure express to use bodyparser as a middleware
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: false}));
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-var incidents = require('./src/incidents.json');
-var creds = require('./src/credentials.json');
+var incidents = require("./src/incidents.json");
+var creds = require("./src/credentials.json");
 
-app.get("/incidents", function(req, res){
-
+app.get("/incidents", function(req, res) {
   res.json(creds);
   res.end();
 });
 
-app.post("/cred", function(req, res){
+//Sort through array of saved cases for cases specific to a authorize agent and return those cases
+function getAgentCases(name) {
+  var cases = {};
+  cases.reports = []; //recreate the incidents database schema
+  incidents.reports.forEach(report => {
+    if (report.agentName === name) {
+      //checks if the log-in agent name matches the agentName property in the record
+      cases.reports.push(report);
+    }
+  });
+  return cases;
+}
+
+app.post("/cred", function(req, res) {
   var userName = req.body.name;
   var pwd = req.body.password;
   var info = {};
-  
+
   info.passFail = false;
 
-  creds.forEach( account => {
-    if(account.username === userName && account.pwd === pwd){
+  //Firt checks if the username and password is in the cedentials.json
+  //If true, returns the name, authorization level, true boolean, and a list of reports based on authoriation.
+  //If false, send the false boolean to the client
+  creds.forEach(account => {
+    if (account.username === userName && account.pwd === pwd) {
       info.username = account.username;
       info.auth = account.auth;
       info.passFail = true;
-      info.reports = incidents;
+      if (account.auth === "supervisor") {
+        info.reports = incidents;
+        getAgentCases(account.username);
+      } else {
+        info.reports = getAgentCases(account.username);
+      }
     }
   });
 
-  res.json(info);
+  res.json(info); //send data to client.
   res.end();
 });
 
