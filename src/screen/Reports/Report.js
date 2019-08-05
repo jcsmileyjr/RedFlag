6b0c9154321f036b884fbaaf67b68828bfeb06cd
JC Smiley <jcsmileyjr@gmail.com>
2019-08-04 22:21:33 -0500

    Fade in animation for reports

--- src/screen/Reports/Report.js
+++ src/screen/Reports/Report.js
@@ -18,9 +18,9 @@ export default function Reports(props) {
                     <Row className="center"><Col className="pageTitleStyle">View Initial Incident Reports</Col></Row>
                     {/*Nav Bar */}
                     <Nav menu={true} countOfCases = {reports.length}/>                    
-                    
+                    {/*className={`playArea ${props.bgColor}`}|||| animation: fadein 2s */}
                     {reports.map((report, index)=>(
-                        <section className="reportStyle" key={index} style={{borderLeft:report.color, borderLeftWidth:"50px", borderLeftStyle:"solid"}}>
+                        <section className="reportStyle fadeReportsIn" key={index} style={{borderLeft:report.color, borderLeftWidth:"50px", borderLeftStyle:"solid"}}>
                             <Row>
                                 <Col xs={{span:5, offset:1}} sm={{span:5, offset:2}} className="lineTitle" >Patron Name:</Col>
                                 <Col xs={4}>{report.patronName}</Col>
