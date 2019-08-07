ad866149726a9b02b5bfc5607db02ea6f93afefd
JC Smiley <jcsmileyjr@gmail.com>
2019-08-06 21:00:01 -0500

    Phase 1, prep server-side code to be deployed to Heroku

--- index.js
+++ index.js
@@ -110,8 +110,7 @@ app.post("/login", function(req, res) {
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get("/", (req, res) => {
-  //res.sendFile(path.join(__dirname + "/build/index.html"));
-  res.send("Hello World!");
+  res.sendFile(path.join(__dirname + "/build/index.html"));
 });
 
 const port = process.env.PORT || 5000;
